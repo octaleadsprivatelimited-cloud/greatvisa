2f69e22ae836f07c529c93dbe99cb66e54d2eb09
Octa Leads <octaleadsprivatelimited@gmail.com>
2025-10-08 22:23:13 +0530

    Move work visa image up by another 15%
    - Changed objectPosition from 'center 35%' to 'center 20%'
    - Total shift from original: 45% upward
    - Shows upper portion of the image

--- client/src/pages/WorkVisa.tsx
+++ client/src/pages/WorkVisa.tsx
@@ -67,7 +67,7 @@ const WorkVisa: React.FC = () => {
             src="/services/word_visa.jpg"
             alt="Work Visa"
             className="w-full h-full object-cover"
-            style={{ objectPosition: 'center 35%' }}
+            style={{ objectPosition: 'center 20%' }}
           />
         </div>
         <div className="absolute inset-0 bg-black/50"></div>
